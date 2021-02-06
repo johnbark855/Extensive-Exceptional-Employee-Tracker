28ba2701366afc3ecf6092a6b6735f7bc804c133
Alex Gold <johnbark855@gmail.com>
2021-02-06 10:49:08 -0500

    uploaded the sql and json files

---
+++ employeeSchema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS company_employees;
+CREATE DATABASE company_employees;
+USE company_employees;
+
+-- create 3 different tables --
+CREATE TABLE department(
+    id INTEGER (4) NOT NULL,
+    dpt_name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+    id INTEGER (4) NOT NULL,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL (9,2) NOT NULL,
+    department_id INTEGER (4) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INTEGER (4) NOT NULL,
+    manager_id INTEGER (6),
+    PRIMARY KEY (id)
+);
+
+-- SELECT * FROM employee; --
+-- SELECT * FROM roles; --
+-- SELECT * FROM department; --
+
+SELECT COUNT(id) FROM employee;
